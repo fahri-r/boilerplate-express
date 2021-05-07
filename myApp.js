d921951a49bf0d4adbe678c4034ae27b257626a5
fahri-r <fahrim42@protonmail.com>
2021-05-07 09:04:38 +0700

    Serve static asset

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res) {
+    app.use("/public", express.static(__dirname + "/public"));
     res.sendFile(__dirname + "/views/index.html");
 });
 
